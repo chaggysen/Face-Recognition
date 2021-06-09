2a6e1c256135460f32052e953bbd55ff23e88944
chaggysen <sen.wang@mail.mcgill.ca>
2021-06-09 19:43:09 -0400

    refactor and screenshots

--- face-recognition-client/src/App.js
+++ face-recognition-client/src/App.js
@@ -29,6 +29,21 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input :"",
+  imageUrl: "",
+  box: {},
+  route: "signin",
+  isSignin: false,
+  user : {
+    id: "",
+    name : "",
+    email : "",
+    entries: 0,
+    joined: ""
+  }
+}
+
 class App extends Component{
   constructor (){
     super()
@@ -100,7 +115,7 @@ class App extends Component{
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries: count}))
           })
-
+          .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -109,7 +124,7 @@ class App extends Component{
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
